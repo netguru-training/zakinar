9410ce8c69a4c103dc7421611df6b8fba436df63
Kubsonix <kuba.sniec@gmail.com>
2015-07-25 17:34:05 +0200

    Show categories on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   expose(:my_categories) { fetch_my_categories }
-  expose(:other_categories) { Category.other_categories(fetch_other_categories) }
+  expose(:other_categories) { Category.other_categories(fetch_other_categories_ids) }
+
+  def index
+    @categories = Category.all unless user_signed_in?
+  end
 
   private
 
