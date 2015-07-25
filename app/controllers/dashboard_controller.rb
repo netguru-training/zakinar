a372d4cbc95568a027dcb3f114f9375a86716d60
Kubsonix <kuba.sniec@gmail.com>
2015-07-25 17:08:23 +0200

    Expose categories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class DashboardController < ApplicationController
-  def index
+  expose(:my_categories) { fetch_my_categories }
+  expose(:other_categories) { Category.other_categories(fetch_other_categories) }
+
+  private
+
+  def fetch_my_categories
+    return [] unless user_signed_in?
+    current_user.categories
+  end
+
+  def fetch_other_categories_ids
+    return [] unless user_signed_in?
+    Category.ids - current_user.category_ids
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   has_many :notes
   has_many :user_categories
   has_many :users , through: :user_categories
+  
+  scope :other_categories, (category_ids)-> { where params[:id] current_user.category_ids }
 end
