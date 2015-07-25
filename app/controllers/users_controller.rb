c0768ba4ff56c5c2e3028786b632a9545e0f3a98
Piotr Pietrusewicz <piotrekp121@gmail.com>
2015-07-25 18:12:13 +0200

    New decorator User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   before_action :admin_only, :except => :show
 
   def index
-    @users = User.all
+    @users = User.all.decorate
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]).decorate
     unless current_user.admin?
       unless @user == current_user
         redirect_to :back, :alert => "Access denied."
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,13 +1,13 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def full_name
+    user_name.presence || email
+  end
 
+  private
+
+  def user_name
+    "#{first_name} #{last_name}"
+  end
 end
