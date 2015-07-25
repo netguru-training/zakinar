b5c8494bae160905e17ec96ce9fe984af4587ac4
Kubsonix <kuba.sniec@gmail.com>
2015-07-25 15:23:32 +0200

    Repair Sign_up & Sign_In View && Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name)
+	end
 end
