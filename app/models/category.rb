3da3c478e1355c695163124a30dc3cc11c79da73
robo-hobo <robohobo-95@o2.pl>
2015-07-25 18:03:19 +0200

    Category - small code redesign

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :user_categories
-  has_many :note_categories
   has_many :users , through: :user_categories
+  has_many :note_categories
+  has_many :notes , through: :note_categories
 
   scope :other_categories, ->(category_ids) { where(id: category_ids) }
-  has_many :notes , through: :note_categories
 end
