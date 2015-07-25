ab286ea0938a073c36990b253478d29ee1e6b280
robo-hobo <robohobo-95@o2.pl>
2015-07-25 17:45:15 +0200

    References to NoteCategory

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :user_categories
+  has_many :note_categories
   has_many :users , through: :user_categories
 
   scope :other_categories, ->(category_ids) { where(id: category_ids) }
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ActiveRecord::Base
+  has_many :note_categories
   has_many :categories, through: :note_categories
   belongs_to :user
 end
