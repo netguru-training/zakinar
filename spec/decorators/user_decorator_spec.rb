6bb5c555ab65b5804a74ecc4a1cb94cfc7ea3ded
Piotr Pietrusewicz <piotrekp121@gmail.com>
2015-07-25 19:09:43 +0200

    New style add && tests

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe UserDecorator do
+
+  describe '#full_name' do
+    subject { user.decorate.full_name }
+
+    context 'first_name & last_name are eq null' do
+      let(:user) { build(:user, email: 'test@test.com', first_name: nil, last_name: nil) }
+      it { is_expected.to eq 'test@test.com' }
+    end
+
+    context 'first_name & last_name are present' do
+      let(:user) { build(:user, first_name: 'John', last_name: 'Doe', email: 'test@test.com') }
+      it { is_expected.to eq 'John Doe' }
+    end
+  end
 end
