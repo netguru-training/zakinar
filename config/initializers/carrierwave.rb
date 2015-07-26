3376e63c026713983782537d0518d8fb0bd78290
Piotr Pietrusewicz <piotrekp121@gmail.com>
2015-07-26 12:01:24 +0200

    Changes in carrierwave and views

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,17 @@
 # config/initializers/carrierwave.rb
-unless %w(development test).include?(Rails.env)
-  CarrierWave.configure do |config|
+
+require 'carrierwave'
+
+CarrierWave.configure do |config|
+  if %w(development test).include?(Rails.env)
+    config.storage = :file
+  else
     config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV["AWS_ACCESS_KEY"],
-        aws_secret_access_key: ENV["AWS_SECRET_KEY"]
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY"],
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"]
     }
+    config.storage = :fog
     config.fog_directory  = ENV["AWS_BUCKET"]
   end
 end
